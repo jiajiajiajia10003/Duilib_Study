2013f05e5ae0487ee709a4c40f4de5a320f1a9bc
jia <jiajiajiajia10003@163.com>
2020-07-03 01:42:22 +0800

    新建：导入Duilib工程

--- Duilib_Study/main.cpp
+++ Duilib_Study/main.cpp
@@ -1,7 +1,19 @@
 #include <windows.h>
 #include <tchar.h>
+
+#include "UIlib.h"
+using namespace DuiLib;
+#ifdef _DEBUG
+#   pragma comment(lib, "../lib/DuiLib/DuiLib_d.lib")
+#else
+#   pragma comment(lib, "DuiLib/DuiLib.lib")
+#endif
+
+
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
-	::MessageBox(NULL, _T("Hello World!"), NULL, NULL);
+	//::MessageBox(NULL, _T("Hello World!"), NULL, NULL);
+	CPaintManagerUI::SetInstance(hInstance);//设置进程的实例句柄，这个值会在注册窗口类时使用
+
 	return 0; 
 }
